58b4b5839963593bf97ad5bdca5631f83c91c926
Producer234 <prexplanation2@gmail.com>
2025-10-24 15:32:46 +0600

    Add files via upload

---
+++ strings/__init__.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2025 by PR-all-bots @ Github, < https://github.com/PR-All-Bots >
+# All rights reserved. © PriyaMusic.
+
+"""
+PriyaMusic is a private Telegram bot project developed for personal use.
+Copyright (c) 2025 ~ Present PR-all-bots <https://github.com/PR-All-Bots>
+
+This program is licensed software: you may use and modify it for personal,
+non-commercial purposes. Collaboration and improvements are welcome
+with proper credit to the original creator.
+"""
+
+
+import os
+from typing import List
+
+import yaml
+
+languages = {}
+commands = {}
+languages_present = {}
+
+
+def get_command(value: str) -> List:
+    return commands["command"][value]
+
+
+def get_string(lang: str):
+    return languages[lang]
+
+
+for filename in os.listdir(r"./strings"):
+    if filename.endswith(".yml"):
+        language_name = filename[:-4]
+        commands[language_name] = yaml.safe_load(
+            open(f"./strings/{filename}", encoding="utf8")
+        )
+
+
+for filename in os.listdir(r"./strings/langs/"):
+    if "en" not in languages:
+        languages["en"] = yaml.safe_load(
+            open(r"./strings/langs/en.yml", encoding="utf8")
+        )
+        languages_present["en"] = languages["en"]["name"]
+    if filename.endswith(".yml"):
+        language_name = filename[:-4]
+        if language_name == "en":
+            continue
+        languages[language_name] = yaml.safe_load(
+            open(f"./strings/langs/{filename}", encoding="utf8")
+        )
+        for item in languages["en"]:
+            if item not in languages[language_name]:
+                languages[language_name][item] = languages["en"][item]
+    try:
+        languages_present[language_name] = languages[language_name]["name"]
+    except Exception:
+        print("There is some issue with the language files.")
+        exit()
