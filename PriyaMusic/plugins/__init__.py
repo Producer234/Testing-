8ae22313044114c820890f519b8d321580096f82
Producer234 <prexplanation2@gmail.com>
2025-10-25 21:38:49 +0600

    Add files via upload

--- PriyaMusic/plugins/__init__.py
+++ PriyaMusic/plugins/__init__.py
@@ -1,1 +1,29 @@
+# Copyright (C) 2025 by PR-all-bots @ Github, < https://github.com/PR-All-Bots >
+# All rights reserved. © PriyaMusic.
 
+"""
+PriyaMusic is a private Telegram bot project developed for personal use.
+Copyright (c) 2025 ~ Present PR-all-bots <https://github.com/PR-All-Bots>
+
+This program is licensed software: you may use and modify it for personal,
+non-commercial purposes. Collaboration and improvements are welcome
+with proper credit to the original creator.
+"""
+
+import glob
+from os.path import dirname, isfile
+
+
+def __list_all_modules():
+    work_dir = dirname(__file__)
+    mod_paths = glob.glob(f"{work_dir}/*/*.py")
+
+    return [
+        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+
+
+ALL_MODULES = sorted(__list_all_modules())
+__all__ = ALL_MODULES + ["ALL_MODULES"]
