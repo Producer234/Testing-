bf1587ea83e552177f7ec6f29947adb262278586
Producer234 <prexplanation2@gmail.com>
2025-10-25 21:48:51 +0600

    Add files via upload

--- PriyaMusic/plugins/misc/autoleave.py
+++ PriyaMusic/plugins/misc/autoleave.py
@@ -1,1 +1,87 @@
+# Copyright (C) 2025 by PR-all-bots @ Github, < https://github.com/PR-All-Bots >
+# All rights reserved. © PriyaMusic.
 
+"""
+PriyaMusic is a private Telegram bot project developed for personal use.
+Copyright (c) 2025 ~ Present PR-all-bots <https://github.com/PR-All-Bots>
+
+This program is licensed software: you may use and modify it for personal,
+non-commercial purposes. Collaboration and improvements are welcome
+with proper credit to the original creator.
+"""
+
+import asyncio
+from datetime import datetime
+from pyrogram.enums import ChatType
+
+import config
+from PriyaMusic import app
+from PriyaMusic.core.call import Alexa, autoend
+from PriyaMusic.utils.database import (
+    get_client,
+    is_active_chat,
+    is_autoend,
+)
+
+autoend = {}
+
+
+async def auto_leave():
+    if config.AUTO_LEAVING_ASSISTANT != str(True):
+        return
+    while not await asyncio.sleep(config.AUTO_LEAVE_ASSISTANT_TIME):
+        from PriyaMusic.core.userbot import assistants
+
+        for num in assistants:
+            client = await get_client(num)
+            try:
+                async for i in client.get_dialogs():
+                    chat_type = i.chat.type
+                    if chat_type in [
+                        ChatType.SUPERGROUP,
+                        ChatType.GROUP,
+                        ChatType.CHANNEL,
+                    ]:
+                        chat_id = i.chat.id
+                        if chat_id not in [
+                            config.LOG_GROUP_ID,
+                            -1001686672798,
+                        ] and not await is_active_chat(chat_id):
+                            try:
+                                await client.leave_chat(chat_id)
+                            except Exception:
+                                continue
+            except Exception:
+                pass
+
+
+asyncio.create_task(auto_leave())
+
+
+# async def auto_end():
+#     while not await asyncio.sleep(30):
+#         if not await is_autoend():
+#             continue
+#         for chat_id in autoend:
+#             timer = autoend.get(chat_id)
+#             if not timer:
+#                 continue
+#             if datetime.now() > timer:
+#                 if not await is_active_chat(chat_id):
+#                     autoend[chat_id] = {}
+#                     continue
+#                 autoend[chat_id] = {}
+#                 try:
+#                     await Alexa.stop_stream(chat_id)
+#                 except Exception:
+#                     continue
+#                 try:
+#                     await app.send_message(
+#                         chat_id,
+#                         "» ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+#                     )
+#                 except Exception:
+#                     continue
+
+
+# asyncio.create_task(auto_end())
---
+++ PriyaMusic/plugins/misc/suggestion.py
@@ -0,0 +1,90 @@
+# Copyright (C) 2025 by PR-all-bots @ Github, < https://github.com/PR-All-Bots >
+# All rights reserved. © PriyaMusic.
+
+"""
+PriyaMusic is a private Telegram bot project developed for personal use.
+Copyright (c) 2025 ~ Present PR-all-bots <https://github.com/PR-All-Bots>
+
+This program is licensed software: you may use and modify it for personal,
+non-commercial purposes. Collaboration and improvements are welcome
+with proper credit to the original creator.
+"""
+
+
+import asyncio
+import random
+from datetime import datetime, timedelta
+
+import config
+from config import clean
+from strings import get_string
+from PriyaMusic import app
+from PriyaMusic.utils.database import (
+    get_lang,
+    get_private_served_chats,
+    get_served_chats,
+    is_suggestion,
+)
+
+LEAVE_TIME = config.AUTO_SUGGESTION_TIME
+
+
+suggestor = {}
+
+strings = [item for item in get_string("en") if item[:3] == "sug" and item != "sug_0"]
+
+
+async def dont_do_this():
+    if config.AUTO_SUGGESTION_MODE != str(True):
+        return
+    while not await asyncio.sleep(LEAVE_TIME):
+        try:
+            chats = []
+            if config.PRIVATE_BOT_MODE == str(True):
+                schats = await get_private_served_chats()
+            else:
+                schats = await get_served_chats()
+            for chat in schats:
+                chats.append(int(chat["chat_id"]))
+            total = len(chats)
+            if total >= 100:
+                total //= 10
+            send_to = 0
+            random.shuffle(chats)
+            for x in chats:
+                if send_to == total:
+                    break
+                if x == config.LOG_GROUP_ID:
+                    continue
+                if not await is_suggestion(x):
+                    continue
+                try:
+                    language = await get_lang(x)
+                    _ = get_string(language)
+                except Exception:
+                    _ = get_string("en")
+                string = random.choice(strings)
+                if previous := suggestor.get(x):
+                    while previous == (string.split("_")[1]):
+                        string = random.choice(strings)
+                suggestor[x] = string.split("_")[1]
+                try:
+                    msg = _["sug_0"] + _[string]
+                    sent = await app.send_message(x, msg)
+                    if x not in clean:
+                        clean[x] = []
+                    time_now = datetime.now()
+                    put = {
+                        "msg_id": sent.message_id,
+                        "timer_after": time_now
+                        + timedelta(minutes=config.CLEANMODE_DELETE_MINS),
+                    }
+                    clean[x].append(put)
+                    send_to += 1
+                except Exception:
+                    pass
+        except Exception:
+            pass
+
+
+asyncio.create_task(dont_do_this())
