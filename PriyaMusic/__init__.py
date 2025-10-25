f21f5992022b44d7830823cc831268a68bc6d2a3
Producer234 <prexplanation2@gmail.com>
2025-10-25 21:25:21 +0600

    Add files via upload

---
+++ PriyaMusic/__init__.py
@@ -0,0 +1,61 @@
+# Copyright (C) 2025 by PR-all-bots @ Github, < https://github.com/PR-All-Bots >
+# All rights reserved. © PriyaMusic.
+
+"""
+PriyaMusic is a private Telegram bot project developed for personal use.
+Copyright (c) 2025 ~ Present PR-all-bots <https://github.com/PR-All-Bots>
+
+This program is licensed software: you may use and modify it for personal,
+non-commercial purposes. Collaboration and improvements are welcome
+with proper credit to the original creator.
+"""
+
+import asyncio
+import sys
+
+from PriyaMusic.core.bot import AlexaBot
+from PriyaMusic.core.dir import dirr
+from PriyaMusic.core.git import git
+from PriyaMusic.core.userbot import Userbot
+from PriyaMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+
+if sys.platform != "win32":
+    try:
+        import uvloop
+
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+        LOGGER(__name__).info("Using Uvloop Event Loop for Enhanced Performance")
+    except ImportError:
+        LOGGER(__name__).warning("Uvloop not found, using default event loop.")
+
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Bot Client
+app = PriyaBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
---
+++ PriyaMusic/misc.py
@@ -0,0 +1,86 @@
+# Copyright (C) 2025 by PR-all-bots @ Github, < https://github.com/PR-All-Bots >
+# All rights reserved. © PriyaMusic.
+
+"""
+PriyaMusic is a private Telegram bot project developed for personal use.
+Copyright (c) 2025 ~ Present PR-all-bots <https://github.com/PR-All-Bots>
+
+This program is licensed software: you may use and modify it for personal,
+non-commercial purposes. Collaboration and improvements are welcome
+with proper credit to the original creator.
+"""
+
+import socket
+import time
+
+import heroku3
+from pyrogram import filters
+
+import config
+from PriyaMusic.core.mongo import mongodb
+
+from .logging import LOGGER
+
+SUDOERS = filters.user()
+
+HAPP = None
+_boot_ = time.time()
+
+
+def is_heroku():
+    return "heroku" in socket.getfqdn()
+
+
+XCB = [
+    "/",
+    "@",
+    ".",
+    "com",
+    ":",
+    "git",
+    "heroku",
+    "push",
+    str(config.HEROKU_API_KEY),
+    "https",
+    str(config.HEROKU_APP_NAME),
+    "HEAD",
+    "master",
+]
+
+
+def dbb():
+    global db
+    db = {}
+    LOGGER(__name__).info("Database Initialized.")
+
+
+async def sudo():
+    global SUDOERS
+    SUDOERS.add(config.OWNER_ID)
+    sudoersdb = mongodb.sudoers
+    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+    sudoers = sudoers["sudoers"] if sudoers else []
+    if config.OWNER_ID not in sudoers:
+        sudoers.append(config.OWNER_ID)
+        await sudoersdb.update_one(
+            {"sudo": "sudo"},
+            {"$set": {"sudoers": sudoers}},
+            upsert=True,
+        )
+    if sudoers:
+        for user_id in sudoers:
+            SUDOERS.add(user_id)
+    LOGGER(__name__).info("Sudoers Loaded.")
+
+
+def heroku():
+    global HAPP
+    if is_heroku and (config.HEROKU_API_KEY and config.HEROKU_APP_NAME):
+        try:
+            Heroku = heroku3.from_key(config.HEROKU_API_KEY)
+            HAPP = Heroku.app(config.HEROKU_APP_NAME)
+            LOGGER(__name__).info("Heroku App Configured")
+        except BaseException:
+            LOGGER(__name__).warning(
+                f"Please make sure your Heroku API Key and Your App name are configured correctly in the heroku."
+            )
