4e9b1f6e5c91e21b1336ded4df08b7b16d7b79ae
Producer234 <prexplanation2@gmail.com>
2025-10-25 21:45:53 +0600

    Add files via upload

--- PriyaMusic/plugins/bot/help.py
+++ PriyaMusic/plugins/bot/help.py
@@ -1,1 +1,100 @@
+# Copyright (C) 2025 by PR-all-bots @ Github, < https://github.com/PR-All-Bots >
+# All rights reserved. © PriyaMusic.
 
+"""
+PriyaMusic is a private Telegram bot project developed for personal use.
+Copyright (c) 2025 ~ Present PR-all-bots <https://github.com/PR-All-Bots>
+
+This program is licensed software: you may use and modify it for personal,
+non-commercial purposes. Collaboration and improvements are welcome
+with proper credit to the original creator.
+"""
+
+
+from typing import Union
+
+from pyrogram import filters, types
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from config import BANNED_USERS
+from strings import get_command, get_string, helpers
+from PriyaMusic import app
+from PriyaMusic.misc import SUDOERS
+from PriyaMusic.utils import help_pannel
+from PriyaMusic.utils.database import get_lang, is_commanddelete_on
+from PriyaMusic.utils.decorators.language import LanguageStart, languageCB
+from PriyaMusic.utils.inline.help import help_back_markup, private_help_panel
+
+### Command
+HELP_COMMAND = get_command("HELP_COMMAND")
+
+
+@app.on_message(filters.command(HELP_COMMAND) & filters.private & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
+async def helper_private(
+    client: app, update: Union[types.Message, types.CallbackQuery]
+):
+    is_callback = isinstance(update, types.CallbackQuery)
+    if is_callback:
+        try:
+            await update.answer()
+        except Exception:
+            pass
+        chat_id = update.message.chat.id
+        language = await get_lang(chat_id)
+        _ = get_string(language)
+        keyboard = help_pannel(_, True)
+        if update.message.photo:
+            await update.message.delete()
+            await update.message.reply_text(_["help_1"], reply_markup=keyboard)
+        else:
+            await update.edit_message_text(_["help_1"], reply_markup=keyboard)
+    else:
+        chat_id = update.chat.id
+        if await is_commanddelete_on(update.chat.id):
+            try:
+                await update.delete()
+            except Exception:
+                pass
+        language = await get_lang(chat_id)
+        _ = get_string(language)
+        keyboard = help_pannel(_)
+        await update.reply_text(_["help_1"], reply_markup=keyboard)
+
+
+@app.on_message(filters.command(HELP_COMMAND) & filters.group & ~BANNED_USERS)
+@LanguageStart
+async def help_com_group(client, message: Message, _):
+    keyboard = private_help_panel(_)
+    await message.reply_text(_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard))
+
+
+@app.on_callback_query(filters.regex("help_callback") & ~BANNED_USERS)
+@languageCB
+async def helper_cb(client, CallbackQuery, _):
+    callback_data = CallbackQuery.data.strip()
+    cb = callback_data.split(None, 1)[1]
+    keyboard = help_back_markup(_)
+    if cb == "hb5":
+        if CallbackQuery.from_user.id not in SUDOERS:
+            return await CallbackQuery.answer(
+                "ᴏɴʟʏ ғᴏʀ ᴏᴡɴᴇʀ ᴀɴᴅ sᴜᴅᴏᴇʀs", show_alert=True
+            )
+        await CallbackQuery.edit_message_text(helpers.HELP_5, reply_markup=keyboard)
+        return await CallbackQuery.answer()
+    try:
+        await CallbackQuery.answer()
+    except Exception:
+        pass
+    if cb == "hb1":
+        await CallbackQuery.edit_message_text(helpers.HELP_1, reply_markup=keyboard)
+    elif cb == "hb2":
+        await CallbackQuery.edit_message_text(helpers.HELP_2, reply_markup=keyboard)
+    elif cb == "hb3":
+        await CallbackQuery.edit_message_text(helpers.HELP_3, reply_markup=keyboard)
+    elif cb == "hb4":
+        await CallbackQuery.edit_message_text(helpers.HELP_4, reply_markup=keyboard)
+    elif cb == "hb7":
+        await CallbackQuery.edit_message_text(helpers.HELP_7, reply_markup=keyboard)
+    elif cb == "hb8":
+        await CallbackQuery.edit_message_text(helpers.HELP_8, reply_markup=keyboard)
